903e9a0d9c5aeb2b3fd76609ebcb94e87cfc2313
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2014-03-15 11:25:28 -0700

    Created SWTopViewController

---
+++ Shockwave DJs/Shockwave DJs/SWTopViewController.h
@@ -0,0 +1,16 @@
+//
+//  SWTopViewController.h
+//  Shockwave DJs
+//
+//  Created by Victor Ilisei on 3/15/14.
+//  Copyright (c) 2014 Tech Genius. All rights reserved.
+//
+
+#import "SWViewController.h"
+
+@interface SWTopViewController : SWViewController
+
+- (void)showLeftMenu;
+- (void)showRightChat;
+
+@end
